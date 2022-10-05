320570daa882259c65cb7ef8d45a48b302be0e3d
unknown <strychowski953@gmail.com>
2022-10-05 22:06:13 +0200

    Add update values

--- finance.py
+++ finance.py
@@ -3,15 +3,18 @@ import sqlite3
 
 class Finance:
     def __init__(self, file_path):
-        self.conn = sqlite3.connect(file_path)
-        self.c = self.conn.cursor()
+        try:
+            self.conn = sqlite3.connect(file_path)
+            self.c = self.conn.cursor()
+        except sqlite3.Error as e:
+            print(e)
 
     def create_new_table(self, year: int):
         """
         Create new table with name of the year
         :param year: The part of table name. The example of the full name: YEAR_2022
         """
-        query = f"""CREATE TABLE YEAR_{year} (
+        query = f"""CREATE TABLE IF NOT EXISTS YEAR_{year} (
                           Month text,
                           Income text,
                           VAT costs text,
@@ -45,6 +48,11 @@ class Finance:
 
         return self.c.fetchone()
 
+    def update_value_in_month(self, year: int, column, old_value, new_value):
+        query = f"UPDATE YEAR_{year} SET {column} = \"{new_value}\" WHERE {column} = \"{old_value}\""
+        self.c.execute(query)
+        self.conn.commit()
+
 
 finance = Finance('finance.db')
 
@@ -54,3 +62,4 @@ table_name = 2022
 finance.create_new_table(table_name)
 finance.add_value_to_table(table_name, all_data)
 finance.read_values_from_month(table_name, "February")
+finance.update_value_in_month(table_name, 'month', 'January', 'March')
